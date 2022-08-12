4eb5ace83cb7260511916ddac1f1d0bf3b1ef793
Karolyne <47931752+KarolyneG@users.noreply.github.com>
2022-08-11 22:21:41 -0500

    Update helloMundo

--- vars/helloMundo.groovy
+++ vars/helloMundo.groovy
@@ -1,5 +1,14 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+//def call(String name = 'human') {
+//  echo "Hello, ${name}."
+//} 
+
+def call() {
+    def msg = sh (
+        returnStdout: true,
+        script: "echo Hello"
+    )
+    msg += " World"
+    return msg
 }
